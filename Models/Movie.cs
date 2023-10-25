627ab76f8af3070db2e202e79bcfa9910a92fc6a
svguillen <svguillen@LAPSTFPE329>
2023-10-25 00:51:09 -0500

    Agregar archivos de proyecto.

---
+++ Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace Stefanini_Project01.Models
+{
+    public class Movie
+    {
+        public int Id {  get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ Models/MovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Stefanini_Project01.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; } = null!;
+    }
+}
